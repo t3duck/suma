c000e69dd82c28d27b05ff04b318e37e85e22bc3
Valentin Zavaleta <22670011@iguala.tecnm.mx>
2023-02-08 07:43:48 -0600

    practica1 terminada - Valentin Zavaleta

---
+++ src/suma.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class suma {
+    public static void main(String[] args) {
+
+
+        int num1 = 2;
+        int num2 = 2;
+
+        int resultado = num1+num2;
+        System.out.println("El resultado es:"+resultado);
+
+    }
+}
+
+
+
+
+
+
+
+
+
